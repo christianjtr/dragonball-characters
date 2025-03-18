373d87d76ceafacf095db94ad3ed1e7292b71343
Christian José Torrealba Rondón <torrealba.christian@gmail.com>
2025-03-18 20:15:38 +0100

    chore: :wrench: Add client and service paths to project configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@clients": resolve(__dirname, "./src/clients"),
+      "@services": resolve(__dirname, "./src/services"),
+    },
+  },
 })

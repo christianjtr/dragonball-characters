7518ec1921e8c0d5f0202e3365d9285a242bb281
Christian José Torrealba Rondón <torrealba.christian@gmail.com>
2025-03-22 20:05:38 +0100

    feat: :sparkles: Add and integrate basic pagination controls

--- src/pages/CharacterHomeScene/CharactersHomeScene.tsx
+++ src/pages/CharacterHomeScene/CharactersHomeScene.tsx
@@ -1,20 +1,55 @@
-import React from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
+import { useSearchParams } from "react-router";
 import { useFetchCharacters } from '@hooks/useFetchCharacters';
-import { CharacterList } from './components';
+import { ALLOWED_PAGE_SIZES, DEFAULT_CURRENT_PAGE, DEFAULT_PAGE_SIZE } from '@app-types/Pagination';
 import { Pagination } from '@components/Pagination/Pagination';
+import { CharacterList } from './components';
 
 export default function CharactersHomeScene(): React.JSX.Element | null {
-    const { characters, meta, links } = useFetchCharacters();
+    const [searchParam, setSearchParams] = useSearchParams();
 
-    if (!characters) {
+    const pageNumberParam = Number(searchParam.get('page'));
+    const pageSizeParam = Number(searchParam.get('limit'));
+
+    const [pageNumber, setPageNumber] = useState<number>(pageNumberParam || DEFAULT_CURRENT_PAGE);
+    const [pageSize, setPageSize] = useState<number>(ALLOWED_PAGE_SIZES.includes(pageSizeParam) ? pageSizeParam : DEFAULT_PAGE_SIZE);
+
+    const { characters, totalPages, currentPage, itemsPerPage, totalItems } = useFetchCharacters(
+        useMemo(() => ({
+            pagination: { page: pageNumber, limit: pageSize }
+        }), [pageNumber, pageSize]),
+    );
+
+    const handleOnChangePage = ((pageNumber: number) => {
+        setPageNumber(pageNumber);
+    });
+
+    const handleOnChangePageSize = ((pageSize: number) => {
+        setPageSize(pageSize);
+    });
+
+    useEffect(() => {
+        setSearchParams({
+            page: String(pageNumber),
+            limit: String(pageSize)
+        });
+    }, [pageNumber, pageSize, setSearchParams]);
+
+    if (!characters || characters.length === 0) {
         return null;
     }
 
     return (
         <React.Fragment>
-            <div>CharactersHomeScene</div>
             <CharacterList characters={characters} />
-            <Pagination meta={meta} links={links} onPageChange={() => { }} />
+            <Pagination
+                totalPages={totalPages}
+                currentPage={currentPage}
+                pageSize={itemsPerPage}
+                totalItems={totalItems}
+                onChangePage={handleOnChangePage}
+                onChangePageSize={handleOnChangePageSize}
+            />
         </React.Fragment>
     );
 }
--- src/types/Pagination.ts
+++ src/types/Pagination.ts
@@ -1,7 +1,7 @@
 export const DEFAULT_PAGE_SIZE = 10;
 export const DEFAULT_CURRENT_PAGE = 1;
 export const DEFAULT_TOTAL_PAGE = 1;
-export const DEFAULT_PAGE_RANGE_DISPLAYED = 5;
+export const ALLOWED_PAGE_SIZES = [5, 10, 15];
 
 export type Pagination = {
     page: number;

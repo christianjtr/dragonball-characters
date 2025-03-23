9ff7dbdac4aa7c3f7d463b002433833fdb67b7d2
Christian José Torrealba Rondón <torrealba.christian@gmail.com>
2025-03-23 22:55:27 +0100

    chore: :wrench: Scroll to top if location changes

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,11 +1,17 @@
-import React, { lazy } from "react";
-import { Routes, Route, Navigate } from "react-router";
+import React, { lazy, useEffect } from "react";
+import { Routes, Route, Navigate, useLocation } from "react-router";
 
 const CharactersHomeScene = lazy(() => import("@pages/CharacterHomeScene/CharactersHomeScene"));
 const CharacterDetailScene = lazy(() => import("@pages/CharacterDetailScene/CharacterDetailScene"));
 const NotFound = lazy(() => import("@components/NotFound/NotFound"));
 
 export const AppRoutes = (): React.JSX.Element => {
+    const location = useLocation();
+
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, [location]);
+
     return (
         <Routes>
             <Route path="/" element={<Navigate to="characters" replace />} />

155a6b2f43d57bbfec583655359fb86d7e4f5639
Christian José Torrealba Rondón <torrealba.christian@gmail.com>
2025-03-24 02:23:40 +0100

    chore: :construction: Adjust vite config file

--- vite.config.ts
+++ vite.config.ts
@@ -3,18 +3,23 @@ import react from '@vitejs/plugin-react'
 import { resolve } from "path";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@clients": resolve(__dirname, "./src/clients"),
-      "@services": resolve(__dirname, "./src/services"),
-      "@hooks": resolve(__dirname, "./src/hooks"),
-      "@pages": resolve(__dirname, "./src/pages"),
-      "@app-types": resolve(__dirname, "./src/types"),
-      "@components": resolve(__dirname, "./src/components"),
-      "@assets": resolve(__dirname, "./src/assets"),
-      "@utils": resolve(__dirname, "./src/utils"),
+export default defineConfig(({ mode }) => {
+  const isProduction = mode === 'production';
+
+  return {
+    plugins: [react()],
+    base: isProduction ? '/' : '/dragonball-characters',
+    resolve: {
+      alias: {
+        "@clients": resolve(__dirname, "./src/clients"),
+        "@services": resolve(__dirname, "./src/services"),
+        "@hooks": resolve(__dirname, "./src/hooks"),
+        "@pages": resolve(__dirname, "./src/pages"),
+        "@app-types": resolve(__dirname, "./src/types"),
+        "@components": resolve(__dirname, "./src/components"),
+        "@assets": resolve(__dirname, "./src/assets"),
+        "@utils": resolve(__dirname, "./src/utils"),
+      },
     },
-  },
-})
+  }
+});

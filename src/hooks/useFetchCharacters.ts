5c3d8dec509ad8c7522bcf186a69a62e7fd1e350
Christian José Torrealba Rondón <torrealba.christian@gmail.com>
2025-03-27 22:18:52 +0100

    chore: Remove unused effect

--- src/hooks/useFetchCharacters.ts
+++ src/hooks/useFetchCharacters.ts
@@ -40,10 +40,6 @@ export const useFetchCharacters = (props: UseFetchCharactersProps) => {
     }, [searchFilters, shouldSortData, sortFields]);
 
     useEffect(() => {
-
-    }, [sortFields]);
-
-    useEffect(() => {
         fetch();
     }, [fetch]);
 

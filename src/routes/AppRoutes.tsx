b995ca0cbb3226157c4faa398b021e6df751fb0c
Christian José Torrealba Rondón <torrealba.christian@gmail.com>
2025-03-27 23:51:37 +0100

    chore: Add url configuration and package json url

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,6 +1,8 @@
 import React, { lazy, useEffect } from "react";
 import { Routes, Route, Navigate, useLocation } from "react-router";
 
+import { APP_BASE_PATH } from "@config/appConfig";
+
 const CharactersHomeScene = lazy(() => import("@pages/CharacterHomeScene/CharactersHomeScene"));
 const CharacterDetailScene = lazy(() => import("@pages/CharacterDetailScene/CharacterDetailScene"));
 const NotFound = lazy(() => import("@components/NotFound/NotFound"));
@@ -14,7 +16,7 @@ export const AppRoutes = (): React.JSX.Element => {
 
     return (
         <Routes>
-            <Route path="/" element={<Navigate to="characters" replace />} />
+            <Route path={APP_BASE_PATH} element={<Navigate to="characters" replace />} />
             <Route path="characters" element={<CharactersHomeScene />} />
             <Route path="characters/:id" element={<CharacterDetailScene />} />
             <Route path="*" element={<NotFound />} />
